3594c2947787df66e6818c480c6e8dce976b7bfb
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2022-06-01 16:05:24 -0300

    Porfolio

--- Amazing_Graph/script.js
+++ Amazing_Graph/script.js
@@ -1,4 +1,15 @@
-function toggleMenu () {
+const btn = document.getElementById('btn');
+
+function toggleMenu (event) {
     const nav = document.getElementById('nav');
-    nav.classList.toggle('active')
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
 }
+
+btn.addEventListener('click', toggleMenu);
