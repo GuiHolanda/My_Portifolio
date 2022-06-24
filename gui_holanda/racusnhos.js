a4e3e9f0d5cdcf1cef6eba3d735d1c0636409ba4
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2022-06-24 19:32:33 -0300

    Menu

--- gui_holanda/racusnhos.js
+++ gui_holanda/racusnhos.js
@@ -1,13 +1,26 @@
-function filteredArray(arr, elem) {
-  let newArr = [];
-  // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].includes(elem) === false) {
-      newArr.push(arr[i]);
-    }
+// function frankenSplice(arr1, arr2, n) {
+//   const newArray = [];
+//   let remaining = 0;
+//   for (let i = 0; i < n; i++) {
+//     newArray[i] = arr2[i];
+//   }
+//   for (let index in arr1) {
+//     newArray[n] = arr1[index];
+//     n++
+//   }
+//   remaining = arr2.length - n + arr1.length;
+//   for (let j = remaining; j < arr2.length; j++){
+//     newArray[j + arr1.length] = arr2[j]
+//   }
+//   return newArray;
+// }
+
+function frankenSplice(arr1, arr2, n) {
+  let newArray = arr2.slice();
+  for (let index in arr1) {
+    newArray.splice(n, 0, arr1[index])
   }
-  // Only change code above this line
-  return newArr;
+  return arr2;
 }
 
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
