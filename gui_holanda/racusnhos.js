da4c446889800cf1c5baa4aff9d27ed99d66e844
GuiHolanda <guilherme.lopes.holanda@hotmail.com>
2022-06-25 14:41:07 -0300

    responsive menu

--- gui_holanda/racusnhos.js
+++ gui_holanda/racusnhos.js
@@ -1,26 +1,10 @@
-// function frankenSplice(arr1, arr2, n) {
-//   const newArray = [];
-//   let remaining = 0;
-//   for (let i = 0; i < n; i++) {
-//     newArray[i] = arr2[i];
-//   }
-//   for (let index in arr1) {
-//     newArray[n] = arr1[index];
-//     n++
-//   }
-//   remaining = arr2.length - n + arr1.length;
-//   for (let j = remaining; j < arr2.length; j++){
-//     newArray[j + arr1.length] = arr2[j]
-//   }
-//   return newArray;
-// }
-
-function frankenSplice(arr1, arr2, n) {
-  let newArray = arr2.slice();
-  for (let index in arr1) {
-    newArray.splice(n, 0, arr1[index])
+function chunkArrayInGroups(arr, size) {
+  const firstArray = [];
+  let parts = arr.length / size;
+  for (let i = 0; i < parts; i++) {
+    firstArray.push(arr.splice(0, size))
   }
-  return arr2;
+  return firstArray;
 }
 
-console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
